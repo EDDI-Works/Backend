4f545d3f0c302bf8e1520c8bec25e71ae007c0a0
silenc3502 <gcccompil3r@gmail.com>
2025-09-11 02:34:39 +0900

    init: Backend

---
+++ src/main/java/com/example/attack_on_monday_backend/AttackOnMondayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.attack_on_monday_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttackOnMondayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttackOnMondayBackendApplication.class, args);
+    }
+
+}
