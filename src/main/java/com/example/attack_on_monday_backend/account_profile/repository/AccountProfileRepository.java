3d6720b4f1ec052d2b0d433d80d137ffea59a0db
silenc3502 <gcccompil3r@gmail.com>
2025-09-17 22:09:14 +0900

    feat: [이상훈] 인증된 사용자가 자신의 계정 정보를 기반으로 프로필 조회 [AREL-17]

--- src/main/java/com/example/attack_on_monday_backend/account_profile/repository/AccountProfileRepository.java
+++ src/main/java/com/example/attack_on_monday_backend/account_profile/repository/AccountProfileRepository.java
@@ -1,5 +1,6 @@
 package com.example.attack_on_monday_backend.account_profile.repository;
 
+import com.example.attack_on_monday_backend.account.entity.Account;
 import com.example.attack_on_monday_backend.account_profile.entity.AccountProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,6 +9,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface AccountProfileRepository extends JpaRepository<AccountProfile, Long> {
+    Optional<AccountProfile> findByAccount(Account account);
+
     @Query("SELECT ap FROM AccountProfile ap WHERE ap.email = :email")
     Optional<AccountProfile> findWithAccountByEmail(@Param("email") String email);
 
