41d9984d39cfa3e34afdcdd3a80ec329734a084c
Hyunsoo Choi <gustn9025@naver.com>
2025-09-25 17:09:46 +0900

    feat: [최현수] 사용자가 인증에 성공하면 카카오 정보를 가지고 인가를 진행합니다 [AREL-26]

--- src/main/java/com/example/attack_on_monday_backend/kakao_authentication/service/KakaoAuthenticationService.java
+++ src/main/java/com/example/attack_on_monday_backend/kakao_authentication/service/KakaoAuthenticationService.java
@@ -1,8 +1,10 @@
 package com.example.attack_on_monday_backend.kakao_authentication.service;
 
+import java.util.Map;
+
 public interface KakaoAuthenticationService {
-
-
     String requestKakaoAuthenticationLink();
+    Map<String, Object> getAccessToken(String code);
+    Map<String, Object> getUserInfo(String accessToken);
 
 }
